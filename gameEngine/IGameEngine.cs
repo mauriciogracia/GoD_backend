9bf82f93f0a86c3df9915757d252bc550de9b820
Mauricio Gracia <mgracia@visionweb.com>
2021-03-05 22:27:08 -0500

    create basic webApi structure, create IGameEngine and a json file with rules for the game

---
+++ gameEngine/IGameEngine.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+public interface IGameEngine {
+    /// <summary>
+    /// Initializes the games engine loading the json file path
+    /// </summary>
+    /// <param name="pathToJsonRules"></param>
+    /// <returns>True when the rules where loaded correctly</returns>
+    public bool Init(String pathToJsonRules);
+
+    /// <summary>
+    /// Gets the possible moves from the game engine
+    /// </summary>
+    /// <returns>A list of posible moves for the game</returns>
+    public List<String> getPossibleMoves() ;
+
+    /// <summary>
+    /// This metod should return
+    /// 
+    /// +1 when movePlayerOne beats movePlayerTwo
+    ///  0 when movePlayerOne ties with movePlayerTwo
+    /// -1 when movePlayerOne loses to movePlayerTwo
+    /// </summary>
+    /// <param name="movePlayerOne">Move made by player one</param>
+    /// <param name="movePlayerTwo">Move made by player two</param>
+    /// <returns></returns>
+    public int determinResult(String movePlayerOne, String movePlayerTwo) ;
+}
