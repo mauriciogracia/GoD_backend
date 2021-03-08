86ac1fea72df7f3db81e750aa3b7f0a1d497c2e3
Mauricio Gracia <mgracia@visionweb.com>
2021-03-08 08:56:18 -0500

    add a custom logger for the backend

---
+++ src/CustomLogger.cs
@@ -0,0 +1,35 @@
+
+using System.IO;
+using System ;
+
+namespace GoD_backend
+{
+    public static class CustomLogger {
+        private static StreamWriter logFile = null;
+
+        public static void Init(string fileName) {
+            try {
+                logFile = new StreamWriter(fileName, true) ;
+                logFile.AutoFlush = true ;
+            }
+            catch {
+                // the log will be kept in the console no need to show this exception
+            }
+        }
+
+        public static void WriteLine(string line) {
+            string logLine ;
+
+            logLine = string.Format("{0}-{1}",DateTime.Now, line) ;
+
+            if(logFile != null)
+            {
+                logFile.WriteLine (logLine) ;
+            }
+            else 
+            {
+                Console.WriteLine(logLine) ;
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -7,15 +7,20 @@ namespace GoD_backend
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
             IGameEngine mge ;
             string filePath ;
+            
+            CustomLogger.Init(Path.Combine(Directory.GetCurrentDirectory(), "backendLog.txt")) ;
 
             filePath = Path.Combine(GameSettings.getGameEngineFolder(), "PaperRockScissors.json");
+            CustomLogger.WriteLine(String.Format("GameSettings: {0}", filePath)) ;
 
             mge = GameEngineFactory.Create(GameEngineType.MoveGameEngine, filePath);
-
+            CustomLogger.WriteLine("GameEngine has been created");
+            
             CreateHostBuilder(args).Build().Run();
         }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
